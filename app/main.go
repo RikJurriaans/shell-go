6bea0008bc74954a8eb6b73da95379969d940719
Rik Jurriaans <rik@jurriaans.org>
2025-08-15 20:53:22 +0100

    implement the exit command

--- app/main.go
+++ app/main.go
@@ -4,15 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+func IncorrectArgumentsSupplied() {
+	fmt.Println("Incorrect number of arguments")
+}
+
 func main() {
-	for true {
+	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		inputString, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
 		if err != nil {
-			panic("Error reading command from the user.")
+			os.Exit(1)
+		}
+
+		var input []string = strings.Split(inputString[:len(inputString)-1], " ")
+		var command = input[0]
+		var arguments = input[1:]
+
+		if command == "exit" {
+			if len(arguments) > 1 || len(arguments) == 0 {
+				IncorrectArgumentsSupplied()
+			}
+			fmt.Println()
+
+			code, err := strconv.Atoi(arguments[0])
+
+			if err != nil {
+				fmt.Println("Error converting string to int")
+			}
+
+			os.Exit(code)
 		}
 
 		fmt.Println(command[:len(command)-1] + ": command not found")
