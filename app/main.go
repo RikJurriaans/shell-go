fedcd945eb30a6f78a6f0dfcc8ac4e7a116cc7ed
Rik Jurriaans <rik@jurriaans.org>
2025-08-16 15:02:52 +0100

    wip: getting the basic file search working

--- app/main.go
+++ app/main.go
@@ -26,15 +26,38 @@ func handleEcho(arguments []string) {
 	fmt.Println(strings.Join(arguments, " "))
 }
 
-func handleType(arguments []string) {
+func handleType(arguments []string, paths []string) {
 	if arguments[0] == "echo" || arguments[0] == "exit" || arguments[0] == "type" {
 		fmt.Println(arguments[0] + " is a shell builtin")
 	} else {
+		for _, path := range paths {
+			dir, err := os.Open(path)
+
+			if err != nil {
+				fmt.Println("Error opening directory:", err)
+			}
+			defer dir.Close()
+
+			files, err := dir.ReadDir(-1)
+			if err != nil {
+				fmt.Println("Error reading directory:", err)
+			}
+
+			for _, file := range files {
+				if file.Name() == arguments[0] {
+					fmt.Println("File found")
+					return
+				}
+			}
+		}
+
 		fmt.Println(arguments[0] + ": not found")
 	}
 }
 
 func main() {
+	path := strings.Split(os.Getenv("PATH"), ":")
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		inputString, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -53,7 +76,7 @@ func main() {
 		case "echo":
 			handleEcho(arguments)
 		case "type":
-			handleType(arguments)
+			handleType(arguments, path)
 		default:
 			fmt.Println(command + ": command not found")
 		}
