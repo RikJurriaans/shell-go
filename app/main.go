c11ec8810696a04d82bb2072d5b036bfc69afaa7
Rik Jurriaans <rik@jurriaans.org>
2025-10-05 09:04:41 +0100

    add parsing function for commands

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -55,7 +56,7 @@ func handleCD(arguments []string) {
 }
 
 func handleEcho(arguments []string) {
-	fmt.Println(strings.Join(arguments, " "))
+	fmt.Println(strings.Join(arguments, ""))
 }
 
 func isFileExecutable(filePath string) (string, bool) {
@@ -117,6 +118,71 @@ func handlePWD(arguments []string) {
 	fmt.Println(path)
 }
 
+func parseCommandLine(command string) ([]string, error) {
+	var args []string
+	state := "start"
+	current := ""
+	quote := "\""
+	escapeNext := true
+	for i := 0; i < len(command); i++ {
+		c := command[i]
+
+		if state == "quotes" {
+			if string(c) != quote {
+				current += string(c)
+			} else {
+				args = append(args, current)
+				current = ""
+				state = "start"
+			}
+			continue
+		}
+
+		if escapeNext {
+			current += string(c)
+			escapeNext = false
+			continue
+		}
+
+		if c == '\\' {
+			escapeNext = true
+			continue
+		}
+
+		if c == '"' || c == '\'' {
+			state = "quotes"
+			quote = string(c)
+			continue
+		}
+
+		if state == "arg" {
+			if c == ' ' || c == '\t' {
+				args = append(args, current)
+				current = ""
+				state = "start"
+			} else {
+				current += string(c)
+			}
+			continue
+		}
+
+		if c != ' ' && c != '\t' {
+			state = "arg"
+			current += string(c)
+		}
+	}
+
+	if state == "quotes" {
+		return []string{}, errors.New(fmt.Sprintf("unclosed quote in command line: %s", command))
+	}
+
+	if current != "" {
+		args = append(args, current)
+	}
+
+	return args, nil
+}
+
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -126,7 +192,11 @@ func main() {
 			os.Exit(1)
 		}
 
-		var input []string = strings.Split(inputString[:len(inputString)-1], " ")
+		input, err := parseCommandLine(inputString[:len(inputString)-1])
+		if err != nil {
+			fmt.Println("Error parsing the command")
+			continue
+		}
 
 		var command = input[0]
 		var arguments = input[1:]
